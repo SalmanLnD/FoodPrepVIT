b6873f7506a206bdad96da819f1081ad8f16c5eb
Muhammed Salman <salman8official@gmail.com>
2025-03-02 17:56:18 +0530

    Added Cart Items object to track count of each item in cart

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,12 +1,32 @@
-import {createContext} from 'react'
+import {createContext,useState} from 'react'
 import {food_list} from '../assets/assets'
 
 export const StoreContext = createContext(null)
 
 const StoreContexProvider = (props)=>{
-    const contextValue={
-        food_list
+    const [cartItems,setCartItems] = useState({})
+
+    const addToCart = (itemId)=>{
+        if(!cartItems[itemId]){
+            setCartItems({...cartItems,[itemId]:1})
+        }
+        else{
+            setCartItems({...cartItems,[itemId]:cartItems[itemId]+1})
+        }
     }
+
+    const removeFromCart = (itemId)=>{
+        setCartItems({...cartItems,[itemId]:cartItems[itemId]-1})
+    }
+
+    const contextValue={
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart        
+    }
+
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
