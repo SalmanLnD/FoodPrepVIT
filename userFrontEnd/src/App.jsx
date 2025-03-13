f33d6c518e1d22051f21ffe81d2561918cd09efd
Muhammed Salman <salman8official@gmail.com>
2025-03-13 21:19:15 +0530

    added media queries and started working with login component

--- userFrontEnd/src/App.jsx
+++ userFrontEnd/src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import {useState} from 'react'
 import Navbar from './components/Navbar/Navbar'
 import {Routes,Route} from 'react-router-dom'
 import Home from './screens/Home/Home'
 import Cart from './screens/Cart/Cart'
 import PlaceOrder from './screens/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
+import LoginPopUp from './components/LoginPopUp/LoginPopUp'
 
 const App = () => {
+  
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+      {showLogin && <LoginPopUp setShowLogin={setShowLogin}/>}
       <div className='app'>
-        <Navbar/>
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/cart" element={<Cart/>}></Route>
